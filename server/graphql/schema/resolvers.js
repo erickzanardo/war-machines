11cbd5ea169b619fe6a6651043b3412c67feb4b6
Erick Zanardo <erick.zanardo@dextra-sw.com>
2018-09-06 10:11:55 -0300

    Dextra brownbag changes

--- server/graphql/schema/resolvers.js
+++ server/graphql/schema/resolvers.js
@@ -19,28 +19,8 @@ const {
 } = require("../../repository/repository");
 
 const resolvers = {
-  Tank: {
-    nation: obj => nations(obj.nation),
-    period: obj => periods(obj.period),
-    variants: obj => tanksByVariant(obj.id),
-  },
-  Aircraft: {
-    nation: obj => nations(obj.nation),
-    period: obj => periods(obj.period),
-    variants: obj => aircraftsByVariant(obj.id),
-  },
-  Nation: {
-    aircrafts: obj => aircraftsByNation(obj.id),
-    tanks: obj => tanksByNation(obj.id),
-  },
   Query: {
-    allNations: (obj, args, context) => nations(),
     allPeriods: (obj, args, context) => periods(),
-
-    allTanks: (obj, args, context) => tanks(),
-    allAircrafts: (obj, args, context) => aircrafts(),
-
-    findTankById: (obj, { id }) => tanks(id),
   }
 }
 
